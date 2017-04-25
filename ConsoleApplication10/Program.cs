f14f8bc94740bdc40c93a3d1de84fe92c708cbdb
tlorek30 <tlorek30@gmail.com>
2017-04-25 13:28:57 +0200

    zarys kodu
    pierwsze kroki

--- ConsoleApplication10/Program.cs
+++ ConsoleApplication10/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication10
 {
+    public class prostokat // definicja klasy.
+    {
+        public int a, b;   // czlonkowie klasy:
+        public int[] tabprost;             
+
+        public int punkty() // Metoda.
+
+        {
+            return 0;
+  
+        }
+
+        public prostokat()     // konstruktor.
+        {
+            a = 0;
+            b = 0;
+            tabprost[2]=0;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+
+            prostokat figura = new prostokat(); // utworzenie obiektu.
+            
+            figura.a = 2; //nadanie wartosci
+            figura.b = 3;
+            figura.tabprost = 0;
+
+            int wynik = prostokat.punkty();   // wywolanie publicznej metody
+            Console.WriteLine(wynik);       //wyswietlenie wyniku
+            Console.ReadLine();
         }
     }
 }
